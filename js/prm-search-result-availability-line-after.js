c9f0c9cc6318dc4ef7debe6f0bbff51e01b6a5ba
Corinna Baksik <cbaksik@gmail.com>
2025-07-02 11:51:57 -0400

    open library changes

--- js/prm-search-result-availability-line-after.js
+++ js/prm-search-result-availability-line-after.js
@@ -4,61 +4,86 @@
 
 
 angular.module('viewCustom')
-    .controller('prmSearchResultAvailabilityLineAfterCtrl',['$timeout','customHathiTrustService','customService','$q','prmSearchService',function ($timeout, customHathiTrustService,customService, $q, prmSearchService) {
+    .controller('prmSearchResultAvailabilityLineAfterCtrl',['$timeout','customHathiTrustService','$q','prmSearchService',function ($timeout, customHathiTrustService, $q, prmSearchService) {
         var vm=this;
-        var custService=customService;
         var chts=customHathiTrustService;
         var prmsv=prmSearchService;
-        vm.OpenLib = {'rtype':'book','isbn':[],'display':false};
+        vm.OpenLib = {'rtype':'book', 'display':false};
         vm.itemPNX={};
 	   vm.hasTOC='';
         vm.hathiTrust={};
         vm.isSerial='';
-        var map;
-        var openLibUrl = 'https://openlibrary.org/api/books?bibkeys=ISBN:';
-        //var tocUrl = 'https://secure.syndetics.com/index.aspx?isbn=9780674055360/xml.xml&client=harvard&type=xw10';
-        // for testing : var tocUrlBad = 'https://secure.syndetics.com/index.aspx?isbn=2939848394/xml.xml&client=harvard&type=xw10';
-
+        var openLibUrl = 'https://openlibrary.org/api/books?bibkeys=';
 
 
         // see if book is in open library
-        vm.findOpenLib=function () {            
-            if (vm.itemPNX.pnx.display.type[0] === vm.OpenLib.rtype && vm.itemPNX.pnx.addata.isbn) {
-                var param={'isbn':'','hasData':false};
-                param.isbn = vm.itemPNX.pnx.addata.isbn[0];
-                var ourTitle = vm.itemPNX.pnx.addata.btitle[0].replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"").normalize("NFKD").replace(/[\u0300-\u036f]/g, "").replace(/\s/g,"").toLowerCase().substring(0,10);
-                //console.log('ours: '+ourTitle);
-                    //fetch(openLibUrl+param.isbn+'&format=json&jscmd=viewapi', {                        
-                // trying jscmd = details to get title in addition to borrow status so i can perform remedial check that it's the same title before presenting link; sometimes the isbn request returns the wrong book b/c openLib is also searching 020$z
-                    fetch(openLibUrl+param.isbn+'&format=json&jscmd=details', {                        
-                        method: 'GET',
-                        headers: {
-                            'Accept': '*/*'
-                          }
-                    })
-                        .then(function (response) {  
-                            return response.json();
-                        })
-                        .then(function (data) { 
-                            var objKey = (Object.keys(data)); 
-                            var objKeyValue = objKey[0]; 
-                            var openLibPreview = data[objKeyValue].preview;                                              
-                            var openLibTitle = data[objKeyValue].details.title.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"").normalize("NFKD").replace(/[\u0300-\u036f]/g, "").replace(/\s/g,"").toLowerCase().substring(0,10); 
-                            //console.log('openlib: '+openLibTitle);                                             
-                            if (openLibPreview === 'borrow' && openLibTitle === ourTitle) {
-                                vm.OpenLib.display = true;
-                                vm.OpenLib.infoURL = data[objKeyValue].info_url;
-                                vm.OpenLib.previewURL = data[objKeyValue].preview_url;   
-                            } 
-                        })
-                        .catch(function (err) {
-                            //console.log("Open Library call did  not work", err);
-                        });
+	vm.findOpenLib=function () {
+		if (vm.itemPNX.pnx.display.type[0] === vm.OpenLib.rtype) {
+			var ourTitle = vm.itemPNX.pnx.addata.btitle[0].replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"").normalize("NFKD").replace(/[\u0300-\u036f]/g, "").replace(/\s/g,"").toLowerCase().substring(0,10);
+			// doing remedial check on title because sometimes API returns wrong book for given identifer
+			if (vm.itemPNX.pnx.addata.isbn) {
+				var param={'isbn':'','hasData':false};
+				param.isbn = vm.itemPNX.pnx.addata.isbn[0];
+					fetch(openLibUrl+'ISBN:'+param.isbn+'&format=json&jscmd=details', 
+					{
+							method: 'GET',
+							headers: {
+								'Accept': '*/*'
+								}
+					})
+					.then(function (response) {  
+						return response.json();
+					})
+					.then(function (data) { 
+						var objKey = (Object.keys(data)); 
+						var objKeyValue = objKey[0]; 
+						var openLibPreview = data[objKeyValue].preview;                                              
+						var openLibTitle = data[objKeyValue].details.title.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"").normalize("NFKD").replace(/[\u0300-\u036f]/g, "").replace(/\s/g,"").toLowerCase().substring(0,10); 
+						//console.log('openlib: '+openLibTitle);                                             
+						if (openLibPreview === 'borrow' && openLibTitle === ourTitle) {
+							vm.OpenLib.display = true;
+							vm.OpenLib.infoURL = data[objKeyValue].info_url;
+							vm.OpenLib.previewURL = data[objKeyValue].preview_url;   
+						} 
+					})
+					.catch(function (err) {
+						//console.log("Open Library call did  not work", err);
+					});
+
+			} else if (vm.itemPNX.pnx.addata.oclcid) {
+				var param={'oclc':'','hasData':false};
+				param.oclc = vm.itemPNX.pnx.addata.oclcid[0].replace(/\D/g,'');
+				fetch(openLibUrl+'OCLC:'+param.oclc+'&format=json&jscmd=details', 
+					{
+							method: 'GET',
+							headers: {
+								'Accept': '*/*'
+								}
+					})
+					.then(function (response) {  
+						return response.json();
+					})
+					.then(function (data) { 
+						var objKey = (Object.keys(data)); 
+						var objKeyValue = objKey[0]; 
+						var openLibPreview = data[objKeyValue].preview;                                              
+						var openLibTitle = data[objKeyValue].details.title.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"").normalize("NFKD").replace(/[\u0300-\u036f]/g, "").replace(/\s/g,"").toLowerCase().substring(0,10); 
+						//console.log('openlib: '+openLibTitle);                                             
+						if (openLibPreview === 'borrow' && openLibTitle === ourTitle) {
+							vm.OpenLib.display = true;
+							vm.OpenLib.infoURL = data[objKeyValue].info_url;
+							vm.OpenLib.previewURL = data[objKeyValue].preview_url;   
+						} 
+					})
+					.catch(function (err) {
+						//console.log("Open Library call did  not work", err);
+					});
+			}
           }
         };
 
 
-        // hathitrust, this is also used for openlibrary since it needs same identifiers, isbn and oclcid
+        // hathitrust
         vm.getHathiTrustData=function () {
             chts.doGet(vm.hathiTrust.isbn, vm.hathiTrust.oclcid)
                 .then(function (data) {
@@ -85,6 +110,12 @@ angular.module('viewCustom')
                 vm.isSerial=false;
             }
             //console.log(vm.isSerial);
+		  // HathiTrust 
+            vm.hathiTrust=chts.validateHathiTrust(vm.itemPNX);
+            vm.hathiTrustItem={};
+            if(vm.hathiTrust.flag) {
+                vm.getHathiTrustData();
+            }
 
 
 
