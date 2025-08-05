c6cd45e0a07908596342c539269744a42ee88684
Corinna Baksik <cbaksik@gmail.com>
2025-08-05 19:49:42 -0400

    mv mapping constants for image components to separate file for easier maintenance

---
+++ js/component-image-key-value-pairs.js
@@ -0,0 +1,73 @@
+// component-image-key-value-pairs.js for rendering VIA components
+(function () {
+  'use strict';
+
+  angular.module('viewCustom')
+	// work level data
+    .constant('mainInfoMapping', [
+			{ sourceKey: 'title', targetKey: 'Title' },
+			{ sourceKey: 'lds14', targetKey: 'Attribution' },
+			{ sourceKey: 'creator', targetKey: 'Creator' },
+			{ sourceKey: 'addtitle', targetKey: 'Variant title' },
+			{ sourceKey: 'edition', targetKey: 'Edition' },
+			{ sourceKey: 'lds07', targetKey: 'Produced' },
+			{ sourceKey: 'format', targetKey: 'Description' },
+			{ sourceKey: 'description', targetKey: 'Summary' },
+			{ sourceKey: 'lds13', targetKey: 'Notes' },
+			{ sourceKey: 'subject', targetKey: 'Subject' },
+			{ sourceKey: 'lds22', targetKey: 'Style' },
+			{ sourceKey: 'genre', targetKey: 'Form / genre' },
+			{ sourceKey: 'lds23', targetKey: 'Culture' },
+			{ sourceKey: 'lds31', targetKey: 'Place' },
+			{ sourceKey: 'identifier', targetKey: 'Identifier' },
+			{ sourceKey: 'lds24', targetKey: 'Related work' },
+			{ sourceKey: 'lds25', targetKey: 'Related information' },
+			{ sourceKey: 'lds44', targetKey: 'Associated name' },
+			{ sourceKey: 'lds27', targetKey: 'Restrictions' },
+			{ sourceKey: 'lds15', targetKey: 'Rights' },
+			{ sourceKey: 'lds26', targetKey: 'Repository' },
+			{ sourceKey: 'creationdate', targetKey: 'Creation date' },
+			{ sourceKey: 'lds01', targetKey: 'HOLLIS number' },
+			{ sourceKey: 'lds64', targetKey: 'Image detail' }
+    ]);
+    	// component level data from lds65
+	//              NOTE
+	// there are 2-levels - component and component flat (was surrogate in old via)
+	// not mapped below but in use: 
+	// u - image href
+	// x - restriction flag
+	// y - caption or thumbnail
+	// this are avail if needed: o e l
+    .constant('keyOrderMap', [
+			{ oldKey: '9', newKey: 'Title' },
+			{ oldKey: '2', newKey: 'Creator' },
+			{ oldKey: '5', newKey: 'Description' },
+			{ oldKey: 'B', newKey: 'Subject' },
+			{ oldKey: '3', newKey: 'Culture' },
+			{ oldKey: 'M', newKey: 'Style' },
+			{ oldKey: 'F', newKey: 'Form / genre' },
+			{ oldKey: 'P', newKey: 'Place' },
+			{ oldKey: '7', newKey: 'Related work' },
+			{ oldKey: '6', newKey: 'Related information' },
+			{ oldKey: 'I', newKey: 'Classification' },
+			{ oldKey: '0', newKey: 'Associated name' },
+			{ oldKey: 'J', newKey: 'Use restrictions' },
+			{ oldKey: '1', newKey: 'Copyright' },
+			{ oldKey: 'Q', newKey: 'Rights' },
+			{ oldKey: '4', newKey: 'Date' },
+			{ oldKey: '8', newKey: 'Repository' },
+			{ oldKey: 'W', newKey: 'ID' },
+
+			{ oldKey: 'T', newKey: 'Title' },
+			{ oldKey: 'A', newKey: 'Creator' },
+			{ oldKey: 'Z', newKey: 'Description' },
+			{ oldKey: 'S', newKey: 'Subject' },
+			{ oldKey: 'G', newKey: 'Form / genre' },
+			{ oldKey: 'H', newKey: 'Classification' },	
+			{ oldKey: 'N', newKey: 'Associated name' },		
+			{ oldKey: 'K', newKey: 'Use restrictions' },
+			{ oldKey: 'C', newKey: 'Copyright' },
+			{ oldKey: 'D', newKey: 'Date' },
+			{ oldKey: 'R', newKey: 'Repository' }
+    ]);
+})();
--- js/custom-view-component.js
+++ js/custom-view-component.js
@@ -6,7 +6,7 @@
 (function () {
 
 	angular.module('viewCustom')
-	.controller('customViewComponentController', [ '$scope','$sce','$mdMedia','prmSearchService','$location','$stateParams', '$element','$timeout','$window', function ($scope,$sce,$mdMedia,prmSearchService,$location,$stateParams, $element, $timeout, $window) {
+	.controller('customViewComponentController', [ '$scope','$sce','$mdMedia','prmSearchService','$location','$stateParams', '$element','$timeout','$window','mainInfoMapping','keyOrderMap', function ($scope,$sce,$mdMedia,prmSearchService,$location,$stateParams, $element, $timeout, $window,mainInfoMapping,keyOrderMap) {
 
 		let vm = this;
 		var sv=prmSearchService;
@@ -25,80 +25,6 @@
 		vm.componentData={}; 
 		vm.mainInfoRender = {};	
 		vm.viewComponent={};
-
-
-		// work-level data
-		// sourceKey = PNX display element
-		const mainInfoMapping = [
-			{ sourceKey: 'title', targetKey: 'Title' },
-			{ sourceKey: 'lds14', targetKey: 'Attribution' },
-			{ sourceKey: 'creator', targetKey: 'Creator' },
-			{ sourceKey: 'addtitle', targetKey: 'Variant title' },
-			{ sourceKey: 'edition', targetKey: 'Edition' },
-			{ sourceKey: 'lds07', targetKey: 'Produced' },
-			{ sourceKey: 'format', targetKey: 'Description' },
-			{ sourceKey: 'description', targetKey: 'Summary' },
-			{ sourceKey: 'lds13', targetKey: 'Notes' },
-			{ sourceKey: 'subject', targetKey: 'Subject' },
-			{ sourceKey: 'lds22', targetKey: 'Style' },
-			{ sourceKey: 'genre', targetKey: 'Form / genre' },
-			{ sourceKey: 'lds23', targetKey: 'Culture' },
-			{ sourceKey: 'lds31', targetKey: 'Place' },
-			{ sourceKey: 'identifier', targetKey: 'Identifier' },
-			{ sourceKey: 'lds24', targetKey: 'Related work' },
-			{ sourceKey: 'lds25', targetKey: 'Related information' },
-			{ sourceKey: 'lds44', targetKey: 'Associated name' },
-			{ sourceKey: 'lds27', targetKey: 'Restrictions' },
-			{ sourceKey: 'lds15', targetKey: 'Rights' },
-			{ sourceKey: 'lds26', targetKey: 'Repository' },
-			{ sourceKey: 'creationdate', targetKey: 'Creation date' },
-			{ sourceKey: 'lds01', targetKey: 'HOLLIS number' },
-			{ sourceKey: 'lds64', targetKey: 'Image detail' }
-		];
-
-		// component data from lds65
-		//              NOTE
-		// note the 2-levels - component and component flat (was surrogate in old via)
-		// 
-		// not mapped below but in use: 
-		// u - image href
-		// x - restriction flag
-		// y - caption or thumbnail
-		// this are avail if needed: o e l
-		const keyOrderMap = [
-			
-			{ oldKey: '9', newKey: 'Title' },
-			{ oldKey: '2', newKey: 'Creator' },
-			{ oldKey: '5', newKey: 'Description' },
-			{ oldKey: 'B', newKey: 'Subject' },
-			{ oldKey: '3', newKey: 'Culture' },
-			{ oldKey: 'M', newKey: 'Style' },
-			{ oldKey: 'F', newKey: 'Form / genre' },
-			{ oldKey: 'P', newKey: 'Place' },
-			{ oldKey: '7', newKey: 'Related work' },
-			{ oldKey: '6', newKey: 'Related information' },
-			{ oldKey: 'I', newKey: 'Classification' },
-			{ oldKey: '0', newKey: 'Associated name' },
-			{ oldKey: 'J', newKey: 'Use restrictions' },
-			{ oldKey: '1', newKey: 'Copyright' },
-			{ oldKey: 'Q', newKey: 'Rights' },
-			{ oldKey: '4', newKey: 'Date' },
-			{ oldKey: '8', newKey: 'Repository' },
-			{ oldKey: 'W', newKey: 'ID' },
-
-			{ oldKey: 'T', newKey: 'Title' },
-			{ oldKey: 'A', newKey: 'Creator' },
-			{ oldKey: 'Z', newKey: 'Description' },
-			{ oldKey: 'S', newKey: 'Subject' },
-			{ oldKey: 'G', newKey: 'Form / genre' },
-			{ oldKey: 'H', newKey: 'Classification' },	
-			{ oldKey: 'N', newKey: 'Associated name' },		
-			{ oldKey: 'K', newKey: 'Use restrictions' },
-			{ oldKey: 'C', newKey: 'Copyright' },
-			{ oldKey: 'D', newKey: 'Date' },
-			{ oldKey: 'R', newKey: 'Repository' }
-
-			];
 
 
 	   /***********************************************/
