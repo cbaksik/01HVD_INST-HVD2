acab6570704ebb8c948311e5593b6305d0e8b820
Corinna Baksik <cbaksik@gmail.com>
2025-10-10 19:07:34 -0400

    improvements to custom adv help

--- js/prm-advanced-search-after.js
+++ js/prm-advanced-search-after.js
@@ -1,10 +1,9 @@
 /**
- * Created by cbaksik on Sep 8 2025.
- * MARC lang helper.
+
  */
 
  angular.module('viewCustom')
- .controller('prmAdvancedSearchAfterCtrl',['$scope','$mdDialog', '$timeout','languageService',function ($scope,$mdDialog, $timeout,languageService) {
+ .controller('prmAdvancedSearchAfterCtrl',['$scope','$log','$mdDialog', '$timeout','languageService',function ($scope,$log,$mdDialog, $timeout,languageService) {
 		 var vm=this;
 
             vm.copyMessage = '';      // The feedback message
@@ -12,46 +11,79 @@
 
             vm.searchText = '';
 
-		 vm.languages = languageService.getLanguages();
-		 //console.log(vm.languages);
-		
+		  vm.languages = []; 
+
+		vm.tabs = [
+				{ id: 'advHelpTabGuide', title: 'Help with Advanced Search'},
+				{ id: 'advHelpTabLang', title: 'Language Codes'},
+				{ id: 'advHelpTabPub', title: 'Place of publication codes'}
+			];
+
+		vm.onTabSelected = function(tab) {
+            $log.info('Selected tab:', tab.title);
+            // Check if it's the language tab AND if the data hasn't been loaded yet.
+            // The !vm.languages.length check prevents re-loading if the user clicks away and comes back.
+            if (tab.id === 'advHelpTabLang' && !vm.languages.length) {
+                $log.info('Loading language codes...');                
+                // Now we call the service to get the data.
+                vm.languages = languageService.getLanguages();
+            }
+		};
+
+		vm.announceDeselected = function(tab) {
+			$log.info('Deselected tab:', tab.title);
+		};
+
+		vm.removeTab = function(tab) {
+			var index = vm.tabs.indexOf(tab);
+			if (index !== -1) {
+				vm.tabs.splice(index, 1);
+				$log.info('Removed tab:', tab.title);
+			}
+		};
+		 		
+		vm.closeActiveTab = function() {
+		vm.selectedIndex = -1;
+		};
+
 		vm.selectLanguage = function(language) {
                 // populate the search box and hide the list
-                vm.searchText = language.name;
-
-			//const codeToCopy = language.code;
-               //   if (!navigator.clipboard) {
-               //       vm.copyStatus = 'error';
-               //       vm.copyMessage = 'Clipboard API not supported by your browser.';
-               //       return;
-               //   }
-
-               //   navigator.clipboard.writeText(codeToCopy).then(function() {
-               //       vm.copyStatus = 'success';
-               //      //  vm.copyMessage = `Copied to clipboard`;
-               //       vm.copyMessage = `Copied code '${codeToCopy}' to clipboard`;
-               //       $timeout(function() {
-               //           vm.copyMessage = '';
-               //       }, 30000);
-
-               //   }).catch(function(err) {
-               //       vm.copyStatus = 'error';
-               //       vm.copyMessage = 'Failed to copy. See console for error.';
-               //       console.error('Could not copy text: ', err);
-               //   });
-	
-            };
+                vm.searchText = language.code;	
+          };
 
 
 		 vm.$onInit=function () {
-			// console.log("prmAdvancedSearchAfterCtrl");
-			// $scope.isCollapsed = true;
-			$scope.isCollapsedLang = true;
-			$scope.isCollapsedHelp = true;
-			$scope.isCollapsedPub = true;
+			vm.selectedIndex = -1;
 		 };
-		 vm.$onChanges=function () {			
-		 };
+
+		vm.copyCodeToClipboard = function(language) {
+			// Stop the event from bubbling up and triggering other clicks if any exist.
+			// event.stopPropagation(); 
+
+			const codeToCopy = language.code;
+
+			// Use the modern Navigator Clipboard API.
+			if (!navigator.clipboard) {
+				vm.copyStatus = 'error';
+				vm.copyMessage = 'Clipboard API not available in your browser.';
+				return;
+			}
+
+			navigator.clipboard.writeText(codeToCopy).then(function() {
+
+				$timeout(function() {
+					vm.copyStatus = 'success';
+					vm.copyMessage = `Copied code '${codeToCopy}' to clipboard`;
+				});
+
+			}).catch(function(err) {
+				$timeout(function() {
+					vm.copyStatus = 'error';
+					vm.copyMessage = 'Failed to copy code. See console for details.';
+				});
+				console.error('Could not copy text: ', err);
+			});
+		};
 
  }]);
 
